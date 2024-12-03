badfd7f2a4761889ef7062c17498434768024b0d
toheed-q <toheedhashmi78@gmail.com>
2024-12-03 21:58:33 +0500

    Add files via upload

---
+++ LABS/MVC/Model.java
@@ -0,0 +1,15 @@
+
+package Model;
+
+
+public class Model {
+    private String data;
+    public String getData(){
+        return data;
+    }
+    public void setData(String data){
+        this.data=data;
+        
+        
+    }
+}
